49740e0c7baf497915d5610bd7469aed5529d403
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-07 13:36:24 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    print("{} ".format(length), end="")
+    if length > 1:
+        print("arguments:")
+        for a in range(1, length + 1):
+            print("{} : {}".format(a, sys.argv[a]))
+    elif length == 1:
+        print("argument:")
+        print("{} : {}".format(1, sys.argv[1]))
+    else:
+        print("arguments.")
+    
