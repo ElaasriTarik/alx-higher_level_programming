9dc08d6c0774ee9177efd47c3ff19866741e1cb9
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-09 13:19:53 +0100

    Done!

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    le = len(my_list) - 1
+    arr = []
+    for x in range(le):
+        if x % 2 == 0:
+            arr.append(True)
+        else:
+            arr.append(False)
+    return (arr)
