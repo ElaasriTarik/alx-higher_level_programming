077a6aa2a3445280a7248677ccc0979d9d8919d3
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-10 23:55:49 +0100

    Done!"

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+int countList(listint_t **head);
+
+int is_palindrome(listint_t **head)
+{
+  listint_t* curr = *head;
+  int list_length = countList(head);
+  int *arr = malloc(sizeof(int) * list_length);
+  int x = 0;
+  int first = 0;
+  while (curr != NULL)
+    {
+      arr[x] = curr->n;
+      x++;
+      curr = curr->next;
+    }
+  x = x - 1;
+  while (first != x && x >= 0)
+    {
+      if (arr[first] != arr[x])
+	return (0);
+      x--;
+      first++;
+    }
+  return (1);
+}
+
+
+int countList(listint_t **head) {
+    int count = 0;
+    listint_t* curr = *head;
+
+    while (curr != NULL) {
+        count++;
+        curr = curr->next;
+    }
+    return (count);
+}
