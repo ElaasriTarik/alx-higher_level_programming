236c41b7d5efb8503728f7b65c17060da3c0901e
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-13 23:34:54 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
 #!/usr/bin/pyhton3
 def best_score(a_dictionary):
-    mx = 0
-    name = ""
     if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
-    else:
-        key = list(a_dictionary)
-        for k in key:
-            if a_dictionary[k] > mx:
-                mx = a_dictionary[k]
-                name = k
+    name = max(a_dictionary, key=a_dictionary.get)
     return (name)
