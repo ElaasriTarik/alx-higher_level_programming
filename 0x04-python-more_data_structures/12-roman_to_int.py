bd8b7ff7ded8f90d67128a2bd6ee635a10d7bd12
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-15 13:41:38 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False or roman_string == None:
+    is_str = True
+    for c in roman_string:
+        if ord(c) < 32 or ord(c) > 126:
+            is_str = False
+            break
+    if is_str == False or roman_string == None:
         return (0)
     roman_obj = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C':100, 'D': 500, 'M': 1000 }
     keys = list(roman_obj)
