1e3c8e55b25aa06a0b2e1f33df83c7fea80737d0
ElaasriTarik <elaasriya2003@gmail.com>
2023-09-25 21:24:23 +0100

    devide list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    res = 0
     list = []
     for x in range(list_length):
+        res = 0
         try:
             res = my_list_1[x] / my_list_2[x]
         except ZeroDivisionError:
