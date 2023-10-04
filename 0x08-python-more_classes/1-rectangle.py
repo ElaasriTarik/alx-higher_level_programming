63067d7fa066b1a068abe9a408abcc1ab42528dc
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-04 14:24:52 +0100

    rect 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,32 +3,36 @@
 
 
 class Rectangle:
+    """ adding width and height private attributes """
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """ prop for width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        """ setter for width """
+        if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
+        """ prop for height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """ setter for height """
+        if isinstance(value, int) is False:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
