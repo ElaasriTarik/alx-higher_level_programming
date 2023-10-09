ec1bd2cfdaf324782c847addd7fd40dfaa3e7b19
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-09 22:23:22 +0100

    is instance of

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" is_kind_of_class """
+
+
 def is_kind_of_class(obj, a_class):
+    """ returns true or false """
+
     if isinstance(obj, a_class):
         return (True)
     return (False)
