bdb616392fde5b2f6aa6afb2f85740313379daf0
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-10 21:13:17 +0100

    Done!

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,13 +3,4 @@
 
 
 def class_to_json(obj):
-    if isinstance(obj, (int, str, bool)):
-        return (obj)
-
-    if isinstance(obj, list):
-        return ([object_to_serializable_dict(item) for item in obj])
-
-    if isinstance(obj, dict):
-        return ({key: object_to_serializable_dict(value) for key, value in obj.items()})
-
-    return (str(obj))
+    return (obj.__dict__)
