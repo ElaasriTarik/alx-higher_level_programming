4f918257806cfe9790ecb4af00fddd79ba9dc2f2
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-10 20:53:36 +0100

    Done!

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""save args to a file """
+import sys
+import os.path
+import json
+
+
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
+
+
+file = "add.item.json"
+if os.path.isfile(file):
+    list = [sys.argv[1:]]
+    load_from_json(file)
+    save_to_json(list, file)
