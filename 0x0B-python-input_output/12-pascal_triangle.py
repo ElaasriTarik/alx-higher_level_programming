2246e641d1c5d8643474face976eb1e45c1d2f67
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-11 21:45:56 +0100

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" making a pascale_triangle function """
+
+
+def pascal_triangle(n):
+    pascal = []
+    if n <= 0:
+        return ([])
+
+    for i in range(n):
+        if i == 0:
+            pascal.append([1])
+            prev = [1]
+            continue
+        if i == 1:
+            pascal.append([1, 1])
+            prev = [1, 1]
+
+        nxt = [1]
+        ps = pascal[i]
+
+        for x in range(len(ps)):
+            y = x + 1
+            length = len(ps)
+            if y < length:
+                nxt.append(ps[x] + ps[y])
+
+        nxt.append(1)
+        pascal.append(nxt)
+
+    return (pascal)
