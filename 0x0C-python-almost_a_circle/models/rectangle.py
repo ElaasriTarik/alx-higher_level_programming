f293e9939366a16b12908dcf101bcd867c8a8b24
ElaasriTarik <elaasriya2003@gmail.com>
2023-10-16 17:20:01 +0100

    unittest

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,11 +9,12 @@ class Rectangle(Base):
     """ rectangle definition """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        ''' init everything '''
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
