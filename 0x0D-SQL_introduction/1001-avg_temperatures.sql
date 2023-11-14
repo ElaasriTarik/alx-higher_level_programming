dc76fd02def481f5dfd3844f791e8ac9cfbc4cbb
ElaasriTarik <elaasriya2003@gmail.com>
2023-11-15 00:38:15 +0100

    avg temp

---
+++ 0x0D-SQL_introduction/1001-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- average for each city --
+SELECT city, AVG(value * 9/5 + 32) AS avg_temp
+FROM temperatures GROUP BY city ORDER BY avg_temp DESC
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- top 3 cities --
+SELECT city FROM temperatures ORDER BY value;
