0c85e79f2d87a4ef307fe8cb8112b80fad68572d
ElaasriTarik <elaasriya2003@gmail.com>
2023-12-11 16:20:16 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+const args = process.argv;
 
-if (argv.length === 2) {
+if (args.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
