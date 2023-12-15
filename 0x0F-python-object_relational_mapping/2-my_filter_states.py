ab7e7195aab8cf5d60a6ec44979a5fe7b0566a39
ElaasriTarik <elaasriya2003@gmail.com>
2023-12-15 21:30:18 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" fetch all states """
+import MySQLdb
+import sys
+
+
+def start(usr, password, database, search):
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=usr,
+        password=password,
+        db=database
+    )
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM states WHERE \
+    name LIKE BINARY '{search}'")
+    data = cur.fetchall()
+    x = 1
+    for row in data:
+        print(row)
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passw = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    start(username, passw, database, search)
