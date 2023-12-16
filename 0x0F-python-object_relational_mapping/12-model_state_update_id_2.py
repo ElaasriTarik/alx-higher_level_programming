03317c1f0bbc10224585a34180173ad43830e4e0
ElaasriTarik <elaasriya2003@gmail.com>
2023-12-16 12:00:26 +0100

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,8 +12,9 @@ def start(username, password, database):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    to_update = session.query(State).where(State.id == 2)
-    
+    to_update = session.query(State).where(State.id == 2) \
+        .order_by(state.id)
+
     to_update.name = "New Mexico"
     session.commit()
 
