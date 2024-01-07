ec8afc488476151e8a2c46a1e28e12b5352931a2
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 23:30:04 +0100

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,15 +4,16 @@ import requests
 from sys import argv
 
 
-username = argv[1]
-token = argv[2]
+if __name__ == "__main__":
+    username = argv[1]
+    token = argv[2]
 
-url = f'https://api.github.com/users/{username}'
-headers = {"Authorization": f"token {token}"}
+    url = f'https://api.github.com/users/{username}'
+    headers = {"Authorization": f"token {token}"}
 
-res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers)
 
-userData = res.json()
-userId = userData.get("id")
+    userData = res.json()
+    userId = userData.get("id")
 
-print(userId)
+    print(userId)
