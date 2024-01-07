767862660e4b96b1ca95a356b4204ee2107aa020
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 22:25:30 +0100

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = {'email': argv[2]}
-
-    with requests.post(url, data=email) as res:
+    req = requests.post(url, data=email)
+    with requests.get(req) as res:
         print(res)
