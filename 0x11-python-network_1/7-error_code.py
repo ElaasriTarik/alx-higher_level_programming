23071fe53901a2e9030f3a84f433ffa9c175f888
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 22:42:00 +0100

    status code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,11 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    try:
-        with requests.get(url) as res:
-            print(res.text)
-
-    except requests.exceptions.RequestException as e:
-        res = requests.get(url)
+    with requests.get(url) as res:
         if res.status_code >= 400:
             print('Error code: {}'.format(res.status_code))
+        else:
+            print(res.text)
