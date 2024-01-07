e0ec82e2e24567b33288c2cb81c0fc7ea3355eba
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 21:21:32 +0100

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""error code"""
+import urllib.request
+import urllib
+from sys import argv
+
+
+url = argv[1]
+req = urllib.request.Request(url)
+
+try:
+    urllib.request.urlopen(req)
+except urllib.error.HTTPError as e:
+    print('Error code: {}'.format(e.code))
