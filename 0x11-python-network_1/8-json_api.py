36fb14255d7913dd3c5035d8096f96fb6f100ddd
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 23:00:47 +0100

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,7 +11,7 @@ if len(argv) == 1:
 else:
     letter = argv[1]
 
-req = requests.get(url, params={'q': letter})
+req = requests.post(url, params={'q': letter})
 r = req.json()
 
 if (r >= 400):
@@ -19,4 +19,4 @@ if (r >= 400):
 elif (r.lenght == 0):
     print('No result')
 else:
-    print('[{}] {}'.format(r["id"], r["name"]))
+    print('[{}] {}'.format(r.get("id"), r.("name")))
