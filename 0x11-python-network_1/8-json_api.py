2725c4fcf0e01d82f0cf34d65c39c7ffbd1dcef6
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 22:58:09 +0100

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""json api"""
+import requests
+from sys import argv
+
+
+url = 'http://0.0.0.0:5000/search_user'
+letter = ""
+if len(argv) == 1:
+    print('No result')
+else:
+    letter = argv[1]
+
+req = requests.get(url, params={'q': letter})
+r = req.json()
+
+if (r >= 400):
+    print("Not a valid JSON")
+elif (r.lenght == 0):
+    print('No result')
+else:
+    print('[{}] {}'.format(r["id"], r["name"]))
