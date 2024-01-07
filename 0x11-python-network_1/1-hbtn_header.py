b9c0bb3689ad2e5aeeb36b04bcc33877aa59e068
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 20:36:52 +0100

    res headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""response header"""
+import urllib.request
+import sys
+
+
+with urllib.request.urlopen(sys.argv[1]) as res:
+    headers = res.info()
+    val = headers.get('X-Request-Id')
+    print(val)
