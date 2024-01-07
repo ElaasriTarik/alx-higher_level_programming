d628d1dcccbe3c1ac835f3a7b65966c5a1b8ad33
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 20:42:50 +0100

    res headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,5 @@ import sys
 
 
 with urllib.request.urlopen(sys.argv[1]) as res:
-    headers = res.info()
-    val = headers.get('X-Request-Id')
+    val = res.headers.get('X-Request-Id')
     print(val)
