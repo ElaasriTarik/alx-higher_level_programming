100cfdab57280f237780470efa2818dadc451215
ElaasriTarik <elaasriya2003@gmail.com>
2024-01-07 21:04:27 +0100

    POST email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,11 +6,11 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    email = {'emai': argv[2]}
+    email = {'email': argv[2]}
     data = urllib.parse.urlencode(email)
-    data2 = data.encode('utf-8')
+    data = data.encode('utf-8')
 
-    req = urllib.request.Request(url, data2)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as res:
         res = res.read().decode('utf-8')
         print(res)
