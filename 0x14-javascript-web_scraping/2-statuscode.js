941bb9fdb42235c5d0d313ad838a297394c9a804
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-20 19:54:04 +0100

    js is the best

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
+const request = require('request');
 const url = process.argv[2];
 
-fetch(url)
-  .then(response => console.log('code:', response.status))
-  .catch(err => console.log(err));
+if (!url) console.log(undefined);
+else {
+  request.head(url, (err, response) => {
+    if (err) console.log(err);
+    else {
+      console.log('code:', response.statusCode);
+    }
+  });
+}
