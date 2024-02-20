d7aad18f873a5b9fc2bfe8100b7743cf6302086e
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-20 21:38:32 +0100

    js is the best

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,21 +5,23 @@ const url = process.argv[2];
 const users = {};
 if (!url) console.log(undefined);
 else {
-  request(url, (err, response) => {
-    if (err) console.log(err);
-    else {
-      const data = JSON.parse(response.body);
-      for (let x = 0; x < data.length; x++) {
-        const user = data[x].userId;
-        if (Object.prototype.hasOwnProperty.call(users, `${user}`)) {
-          if (data[x].completed === true) {
-            users[`${user}`] += 1;
-          }
-        } else {
-          users[`${user}`] = 0;
-        }
-      }
-      console.log(users);
-    }
-  });
+	request(url, (err, response) => {
+		if (err) console.log(err);
+		else {
+			const data = JSON.parse(response.body);
+			for (let x = 0; x < data.length; x++) {
+				const user = data[x].userId;
+				if (Object.prototype.hasOwnProperty.call(users, `${user}`)) {
+					if (data[x].completed === true) {
+						users[`${user}`] += 1;
+						if (users[`${user}`] === 0) delete users[`${user}`];
+					}
+				} else {
+					users[`${user}`] = 0;
+				}
+
+			}
+			console.log(users);
+		}
+	});
 }
